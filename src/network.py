80b3a16de8d76a9b0d2351dcb9385abf65f08ad4
jklj077 <jklj077@gmail.com>
2016-05-21 00:14:18 +0800

    refactor net id manager: now a class and manage pools on demand

--- src/network.py
+++ src/network.py
@@ -274,6 +274,93 @@ class UserPool(EnumPool):
         print("user netid:"+str(self.netid)+",  net info:"+self.info+",  gateway:"+self.gateway)
         print(str(self.pool))
 
+class NetIdMgr(object):
+    def __init__(self, etcdclient, mode):
+        self.__etcd = etcdclient
+        self.__netid_count = 0
+        self.__pool_count = 0
+        self.__cur_pool_index = -1
+        self.__cur_pool = []
+        if mode == 'new':
+            logger.info("init net id manager")
+        elif mode == 'recovery':
+            logger.info("init net id manager from etcd")
+            self.__load_info()
+            self.__load_pool()
+        else:
+            logger.error("__init__ in NetIdMgr: mode: %s not supported" % (mode))
+            sys.exit(1)
+
+    def __dump_info(self):
+        self.__etcd.setkey('netids/info', json.dump({'netid_count': self.__netid_count, 'pool_count': self.__pool_count}))
+
+    def __load_info(self):
+        [sts, res] = self.__etcd.getkey('netids/info')
+        if sts:
+            info = json.loads(res)
+            self.__netid_count = int(info['netid_count'])
+            self.__pool_count = int(info['pool_count'])
+        else:
+            self.__netid_count = 0
+            self.__pool_count = 0
+            logger.warning('load_info in NetIdMgr: etcd get info failed, use default')
+
+    def __dump_cur_pool(self):
+        if self.__cur_pool_index >=0 and self.__cur_pool_index < self.__pool_count:
+            self.__etcd.setkey('netids/cur_pool_index', self.__cur_pool_index)
+            self.__etcd.setkey('netids/pools/'+str(self.__cur_pool_index), self.__cur_pool)
+
+    def __load_cur_pool_index(self):
+        [sts, res] = self.__etcd.getkey('netids/cur_pool_index')
+        if sts:
+            self.__cur_pool_index = int(res)
+        else:
+            self.__cur_pool_index = -1
+            logger.warning('load_info in NetIdMgr: etcd get cur_pool_index failed, use default')
+
+    def __load_pool(self, pool_index):
+        [sts, res] = self.__etcd.getkey('netids/pools/'+str(pool_index))
+        if sts:
+            self.__cur_pool = list(json.loads(res))
+        else:
+            self.__cur_pool = []
+            logger.warning('load_pool in NetIdMgr: etcd get pools/%s failed, used default' % (str(index)))
+
+    # add pools to etcd, modify etcd info and pools
+    def add_pool(self, count=4094, pool_size=89):
+        new_pool_count = int(celling(count/pool_size))
+        for i in range(0, new_pool_count):
+            pool = list(range(self.__netid_count + i*pool_size, self.__netid_count + (i+1)*pool_size))
+            self.etcd.setkey('netids/pools/'+str(self.__pool_count+i), json.dumps(pool))
+        self.__netid_count += count
+        self.__pool_count += new_pool_count
+        self.__dump_info()
+
+    # get netid, if current pool is empty, load next pool
+    def get_netid(self):
+        if len(self.__cur_pool) == 0:
+            self.__cur_pool_index += 1
+            if self.__cur_pool_index == self.__pool_count:
+                self.__add_pool()
+            self.__load_pool(self.cur_pool_index)
+        netid = self.__cur_pool.pop()
+        self.__dump_cur_pool()
+        return int(netid)
+    
+    # return netid, if current pool is full, load previous pool
+    def ret_netid(self, netid, pool_size=89):
+        if len(self.__cur_pool) == pool_size:
+            self.__cur_pool_index -= 1
+            if self.__cur_pool_index == -1:
+                # should should not happen
+                self.__cur_pool_index += 1
+                logger.warning('ret_netid in NetIdMgr: return netid %s when all pools are full' % (str(netid)))
+            else:
+                self.__load_pool(self.__cur_pool_index)
+        self.__cur_pool.append(netid)
+        self.__dump_cur_pool()
+        return
+
 # NetworkMgr : mange docklet network ip address
 #   center : interval pool to allocate and free network block with IP/CIDR
 #   system : enumeration pool to acquire and release system ip address
@@ -281,6 +368,7 @@ class UserPool(EnumPool):
 class NetworkMgr(object):
     def __init__(self, vnet_count, addr_cidr, etcdclient, mode):
         self.etcd = etcdclient
+        self.__idmgr = NetIdMgr(self.etcd, mode)
         if mode == 'new':
             logger.info("init network manager with %s and max %s ids" % (addr_cidr, str(vnet_count))
             self.center = IntervalPool(addr_cidr=addr_cidr)
@@ -294,9 +382,9 @@ class NetworkMgr(object):
             # But, EnumPool drop the last IP address in its pool -- it is not important
             self.system = EnumPool(sysaddr+"/"+str(syscidr))
             self.users = {}
-            self.netids = {}
-            self.init_netids(vnet_count, 60)
-            self.init_shared_netids()
+            #self.netids = {}
+            #self.init_netids(vnet_count, 60)
+            #self.init_shared_netids()
             self.dump_center()
             self.dump_system()
         elif mode == 'recovery':
@@ -304,74 +392,74 @@ class NetworkMgr(object):
             self.center = None
             self.system = None
             self.users = {}
-            self.netids = {}
+            #self.netids = {}
             self.load_center()
             self.load_system()
-            self.load_netids()
-            self.load_shared_netids()
+            #self.load_netids()
+            #self.load_shared_netids()
         else:
             logger.error("mode: %s not supported" % mode)
 
-    def init_netids(self, total, block):
-        self.netids['total'] = total
-        self.netids['block'] = block
-        self.etcd.setkey("network/netids/info", str(total)+"/"+str(block))
-        i = 0
-        for i in range(1, int((total-1)/block)):
-            self.etcd.setkey("network/netids/"+str(i), json.dumps(list(range(block*(i-1), block*i))))
-        self.netids['currentpool'] = list(range(block*i, total))
-        self.netids['currentindex'] = i+1
-        self.etcd.setkey("network/netids/"+str(i+1), json.dumps(self.netids['currentpool']))
-        self.etcd.setkey("network/netids/current", str(i+1))
+    # def init_netids(self, total, block):
+    #     self.netids['total'] = total
+    #     self.netids['block'] = block
+    #     self.etcd.setkey("network/netids/info", str(total)+"/"+str(block))
+    #     i = 0
+    #     for i in range(1, int((total-1)/block)):
+    #         self.etcd.setkey("network/netids/"+str(i), json.dumps(list(range(block*(i-1), block*i))))
+    #     self.netids['currentpool'] = list(range(block*i, total))
+    #     self.netids['currentindex'] = i+1
+    #     self.etcd.setkey("network/netids/"+str(i+1), json.dumps(self.netids['currentpool']))
+    #     self.etcd.setkey("network/netids/current", str(i+1))
 
     # Data Structure:
     # shared_netids = [{netid = ..., remainnum = ...}, {netid = ..., remainnum = ...}, ...]
-    def init_shared_netids(self, netnum=128, sharenum=128):
-        self.shared_netids = []
-        for i in range(netnum):
-            shared_netid = {}
-            [status, shared_netid['netid']] = self.acquire_netid()
-            if not status:
-                logger.info('not enough netids for sharing, still need %s' % str(netnum-i))
-                break
-            shared_netid['remainnum'] = sharenum
-            self.shared_netids.append(shared_netid)
-        self.etcd.setkey("network/shared_netids", json.dumps(self.shared_netids))
+    # def init_shared_netids(self, netnum=128, sharenum=128):
+    #     self.shared_netids = []
+    #     for i in range(netnum):
+    #         shared_netid = {}
+    #         [status, shared_netid['netid']] = self.acquire_netid()
+    #         if not status:
+    #             logger.info('not enough netids for sharing, still need %s' % str(netnum-i))
+    #             break
+    #         shared_netid['remainnum'] = sharenum
+    #         self.shared_netids.append(shared_netid)
+    #     self.etcd.setkey("network/shared_netids", json.dumps(self.shared_netids))
 
     # load net info and an avaliable net pool
-    def load_netids(self):
-        [status, info] = self.etcd.getkey("network/netids/info")
-        if not status:
-            logger.info("load netids info failed")
-        self.netids['total'] = int(info.split("/")[0])
-        self.netids['block'] = int(info.split("/")[1])
-        [status, current] = self.etcd.getkey("network/netids/current")
-        self.netids['currentindex'] = int(current)
-        if self.netids['currentindex'] == 0:
-            self.netids['currentpool'] = []
-        else:
-            [status, pool] = self.etcd.getkey("network/netids/"+str(self.netids['currentindex']))
-            self.netids['currentpool'] = json.loads(pool)
+    # def load_netids(self):
+    #     [status, info] = self.etcd.getkey("network/netids/info")
+    #     if not status:
+    #         logger.info("load netids info failed")
+    #     self.netids['total'] = int(info.split("/")[0])
+    #     self.netids['block'] = int(info.split("/")[1])
+    #     [status, current] = self.etcd.getkey("network/netids/current")
+    #     self.netids['currentindex'] = int(current)
+    #     if self.netids['currentindex'] == 0:
+    #         self.netids['currentpool'] = []
+    #     else:
+    #         [status, pool] = self.etcd.getkey("network/netids/"+str(self.netids['currentindex']))
+    #         self.netids['currentpool'] = json.loads(pool)
 
-    def dump_netids(self):
-        if self.netids['currentpool'] == []:
-            if self.netids['currentindex'] != 0:
-                self.etcd.delkey("network/netids/"+str(self.netids['currentindex']))
-                self.etcd.setkey("network/netids/current", str(self.netids['currentindex']-1))
-            else:
-                pass
-        else:
-            self.etcd.setkey("network/netids/"+str(self.netids['currentindex']), json.dumps(self.netids['currentpool']))
+    # def dump_netids(self):
+    #     if self.netids['currentpool'] == []:
+    #         if self.netids['currentindex'] != 0:
+    #             self.etcd.delkey("network/netids/"+str(self.netids['currentindex']))
+    #             self.etcd.setkey("network/netids/current", str(self.netids['currentindex']-1))
+    #         else:
+    #             pass
+    #     else:
+    #         self.etcd.setkey("network/netids/"+str(self.netids['currentindex']), json.dumps(self.netids['currentpool']))
 
-    def load_shared_netids(self):
-        [status, shared_netids] = self.etcd.getkey("network/shared_netids")
-        if not status:
-            self.init_shared_netids()
-        else:
-            self.shared_netids = json.loads(shared_netids)
+    # def load_shared_netids(self):
+    #     [status, shared_netids] = self.etcd.getkey("network/shared_netids")
+    #     if not status:
+    #         self.init_shared_netids()
+    #     else:
+    #         self.shared_netids = json.loads(shared_netids)
 
-    def dump_shared_netids(self):
-        self.etcd.setkey("network/shared_netids", json.dumps(self.shared_netids))
+    # def dump_shared_netids(self):
+    #     self.etcd.setkey("network/shared_netids", json.dumps(self.shared_netids))
 
     def load_center(self):
         [status, centerdata] = self.etcd.getkey("network/center")
@@ -417,42 +505,42 @@ class NetworkMgr(object):
         print("<netids>")
         print(str(self.netids['currentindex'])+":"+str(self.netids['currentpool']))
 
-    def acquire_netid(self, isshared=False):
-        if isshared:
-            # only share netid of the front entry
-            # if remainnum is reduced to 0, move the front entry to the back
-            # if remainnum is still equal to 0, one round of sharing is complete, start another one
-            if self.shared_netids[0]['remainnum'] == 0:
-                self.shared_netids.append(self.shared_netids.pop(0))
-            if self.shared_netids[0]['remainnum'] == 0:
-                logger.info("shared netids not enough, add user to full netids")
-                for shared_netid in self.shared_netids:
-                    shared_netid['remainnum'] = 128
-            self.shared_netids[0]['remainnum'] -= 1
-            self.dump_shared_netids()
-            return [True, self.shared_netids[0]['netid']]
+    # def acquire_netid(self, isshared=False):
+    #     if isshared:
+    #         # only share netid of the front entry
+    #         # if remainnum is reduced to 0, move the front entry to the back
+    #         # if remainnum is still equal to 0, one round of sharing is complete, start another one
+    #         if self.shared_netids[0]['remainnum'] == 0:
+    #             self.shared_netids.append(self.shared_netids.pop(0))
+    #         if self.shared_netids[0]['remainnum'] == 0:
+    #             logger.info("shared netids not enough, add user to full netids")
+    #             for shared_netid in self.shared_netids:
+    #                 shared_netid['remainnum'] = 128
+    #         self.shared_netids[0]['remainnum'] -= 1
+    #         self.dump_shared_netids()
+    #         return [True, self.shared_netids[0]['netid']]
 
-        if self.netids['currentpool'] == []:
-            if self.netids['currentindex'] == 0:
-                return [False, "No Net IDs"]
-            else:
-                logger.error("netids current pool is empty with current index not zero")
-                return [False, "internal error"]
-        netid = self.netids['currentpool'].pop()
-        self.dump_netids()
-        if self.netids['currentpool'] == []:
-            self.load_netids()
-        return [True, netid]
+    #     if self.netids['currentpool'] == []:
+    #         if self.netids['currentindex'] == 0:
+    #             return [False, "No Net IDs"]
+    #         else:
+    #             logger.error("netids current pool is empty with current index not zero")
+    #             return [False, "internal error"]
+    #     netid = self.netids['currentpool'].pop()
+    #     self.dump_netids()
+    #     if self.netids['currentpool'] == []:
+    #         self.load_netids()
+    #     return [True, netid]
 
-    def release_netid(self, netid):
-        if len(self.netids['currentpool']) == self.netids['block']:
-            self.netids['currentpool'] = [netid]
-            self.netids['currentindex'] = self.netids['currentindex']+1
-            self.dump_netids()
-        else:
-            self.netids['currentpool'].append(netid)
-            self.dump_netids()
-        return [True, "Release Net ID success"]
+    # def release_netid(self, netid):
+    #     if len(self.netids['currentpool']) == self.netids['block']:
+    #         self.netids['currentpool'] = [netid]
+    #         self.netids['currentindex'] = self.netids['currentindex']+1
+    #         self.dump_netids()
+    #     else:
+    #         self.netids['currentpool'].append(netid)
+    #         self.dump_netids()
+    #     return [True, "Release Net ID success"]
 
     def add_user(self, username, cidr, isshared=False):
         logger.info("add user %s with cidr=%s" % (username, str(cidr)))
@@ -462,7 +550,8 @@ class NetworkMgr(object):
         self.dump_center()
         if not status:
             return [False, result]
-        [status, netid] = self.acquire_netid(isshared)
+        #[status, netid] = self.acquire_netid(isshared)
+        netid = self.__idmgr.get_netid()
         if status:
             netid = int(netid)
         else:
@@ -485,8 +574,9 @@ class NetworkMgr(object):
         logger.info("delete user %s with cidr=%s" % (username, int(cidr)))
         self.center.free(addr, int(cidr))
         self.dump_center()
-        if not isshared:
-            self.release_netid(self.users[username].netid)
+        #if not isshared:
+        #    self.release_netid(self.users[username].netid)
+        self.__idmgr.ret_netid(netid)
         netcontrol.del_gw(self.users[username].switchid, username)
         self.etcd.deldir("network/users/"+username)
         del self.users[username]
